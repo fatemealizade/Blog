a71c30e0c23b37585ecd98dd91bda8a3fcadbd13
fateme alizade <alizadeh.fatemeh@yahoo.com>
2019-08-18 22:23:55 +0430

    get string data from api and convert to post array

--- app/src/main/java/com/example/blog/data/DataHandler.java
+++ app/src/main/java/com/example/blog/data/DataHandler.java
@@ -1,30 +1,17 @@
 package com.example.blog.data;
 
 import com.example.blog.models.Post;
+import com.example.blog.networking.ApiClient;
+import com.google.gson.Gson;
 
 public class DataHandler {
 
     public Post[] getAllPosts() {
-        return new Post[]{
-                new Post() {{
-                    id = 1;
-                    userId = 1;
-                    title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit";
-                    body = "quia et suscipit\\\\nsuscipit recusandae consequuntur expedita et cum\\\\nreprehenderit molestiae ut ut quas totam\\\\nnostrum rerum est autem sunt rem eveniet architecto";
-                }},
-                new Post() {{
-                    id = 2;
-                    userId = 2;
-                    title = "qui est esse";
-                    body = "est rerum tempore vitae\\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\\nqui aperiam non debitis possimus qui neque nisi nulla";
-                }},
-                new Post() {{
-                    id = 3;
-                    userId = 3;
-                    title = "ea molestias quasi exercitationem repellat qui ipsa sit aut";
-                    body = "et iusto sed quo iure\\nvoluptatem occaecati omnis eligendi aut ad\\nvoluptatem doloribus vel accusantium quis pariatur\\nmolestiae porro eius odio et labore et velit aut";
-                }}
-        };
+        ApiClient apiClient = new ApiClient();
+        String responseStr = apiClient.getAllPosts();
+
+        Gson gson = new Gson();
+        return gson.fromJson(responseStr, Post[].class);
     }
 
 }
