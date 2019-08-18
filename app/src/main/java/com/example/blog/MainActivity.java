b0e4ebbbc4e60d098d5e33c4c8260a9d4c7b9b1c
fateme alizade <alizadeh.fatemeh@yahoo.com>
2019-08-18 20:32:22 +0430

    add posts ListView adapter boilerplate

--- app/src/main/java/com/example/blog/MainActivity.java
+++ app/src/main/java/com/example/blog/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.blog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import com.example.blog.data.DataHandler;
+import com.example.blog.models.Post;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView postsLst = findViewById(R.id.posts_lst);
+        postsLst.setAdapter(new PostsAdapter());
     }
+
+    class PostsAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
+
 }
